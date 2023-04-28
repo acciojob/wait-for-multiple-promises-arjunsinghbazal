24fcb0a10cc94aabbe06278dd0b72c2bc34ed0f2
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-04-28 20:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,52 @@
-//your JS code here. If required.
-// Get the table body element
-const tableBody = document.querySelector("#output");
+const res = document.getElementById("output");
 
-// Create an array with 3 Promises
 const promises = [
-  createRandomPromise(),
-  createRandomPromise(),
-  createRandomPromise()
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
+  }),
 ];
 
-// Add a loading row to the table
-const loadingRow = document.createElement("tr");
-const loadingCell = document.createElement("td");
-loadingCell.setAttribute("colspan", "2");
-loadingCell.textContent = "Loading...";
-loadingRow.appendChild(loadingCell);
-tableBody.appendChild(loadingRow);
+async function callFns() {
+  const start = new Date();
+  // Use Promise.all to wait for all Promises to resolve
+  res.innerHTML += `
+            <tr id="loading">
+                <td colspan=2>Loading...</td>
+            </tr>
+          `;
+  await Promise.all(promises)
+    .then((results) => {
+      res.innerHTML = ``;
+      // Log the array of results
+      results.forEach((e) => {
+        res.innerHTML += `
+            <tr>
+                <td>${e.name}</td>
+                <td>${e.time}</td>
+            </tr>
+          `;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-// Wait for all the Promises to resolve using Promise.all
-Promise.all(promises)
-  .then(results => {
-    // Remove the loading row from the table
-    tableBody.removeChild(loadingRow);
-
-    // Populate the table with the Promise results
-    for (let i = 0; i < results.length; i++) {
-      const result = results[i];
-      const row = document.createElement("tr");
-      const nameCell = document.createElement("td");
-      nameCell.textContent = `Promise ${i + 1}`;
-      const timeCell = document.createElement("td");
-      timeCell.textContent = `${result.toFixed(3)}`;
-      row.appendChild(nameCell);
-      row.appendChild(timeCell);
-      tableBody.appendChild(row);
-    }
-
-    // Add a row for the total time taken
-    const totalTime = results.reduce((acc, curr) => acc + curr, 0);
-    const totalRow = document.createElement("tr");
-    const totalNameCell = document.createElement("td");
-    totalNameCell.textContent = "Total";
-    const totalTimeCell = document.createElement("td");
-    totalTimeCell.textContent = `${totalTime.toFixed(3)}`;
-    totalRow.appendChild(totalNameCell);
-    totalRow.appendChild(totalTimeCell);
-    tableBody.appendChild(totalRow);
-  });
-
-// Helper function to create a Promise that resolves after a random time between 1 and 3 seconds
-function createRandomPromise() {
-  const randomTime = Math.floor(Math.random() * 3000) + 1000;
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(randomTime / 1000);
-    }, randomTime);
-  });
+  const end = new Date();
+  const timeInMillis = end - start;
+  res.innerHTML += `
+            <tr>
+                <td>Total</td>
+                <td>${timeInMillis / 1000}</td>
+            </tr>
+          `;
 }
+callFns();
